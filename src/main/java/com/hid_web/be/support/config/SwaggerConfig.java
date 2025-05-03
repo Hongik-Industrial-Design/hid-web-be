0ab172355e54c10e7a89068b6fe4229ae5b11e59
jypark <jyp8637282@gmail.com>
2025-05-03 22:52:23 +0900

    hotfix: swagger server url 고정

--- src/main/java/com/hid_web/be/support/config/SwaggerConfig.java
+++ src/main/java/com/hid_web/be/support/config/SwaggerConfig.java
@@ -5,12 +5,16 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "HID API", version = "v1"),
-        security = @SecurityRequirement(name = "bearerAuth")
+        security = @SecurityRequirement(name = "bearerAuth"),
+        servers = {
+                @Server(url = "https://api.hongik-sandi.kr")
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
